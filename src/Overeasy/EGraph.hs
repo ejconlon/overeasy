516a4d4dac36a60ec1e6036866476a7ef64f002e
Eric Conlon <ejconlon@gmail.com>
2021-10-10 16:33:42 -0700

    simplify

--- src/Overeasy/EGraph.hs
+++ src/Overeasy/EGraph.hs
@@ -8,9 +8,6 @@ module Overeasy.EGraph
   , ENodeId
   , EAnalysis (..)
   , EAnalysisOff (..)
-  , EGC
-  , EGM
-  , runEGM
   , EGraph
   , egNew
   , egAddTerm
@@ -20,8 +17,7 @@ module Overeasy.EGraph
   ) where
 
 import Control.DeepSeq (NFData)
-import Control.Monad.Reader (MonadReader, ask)
-import Control.Monad.State.Strict (MonadState, get, gets, modify')
+import Control.Monad.State.Strict (State, get, gets, modify')
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HashMap
 import Data.HashSet (HashSet)
@@ -34,7 +30,7 @@ import Overeasy.Assoc (Assoc, assocEnsure, assocNew)
 import Overeasy.Classes (BoundedJoinSemilattice, Changed (..))
 import Overeasy.Recursion (RecursiveWhole, foldWholeTrackM)
 import Overeasy.Source (Source, sourceAdd, sourceNew)
-import Overeasy.StateUtil (RSM, runRSM, stateLens)
+import Overeasy.StateUtil (stateLens)
 import Overeasy.UnionFind (UnionFind, ufFind, ufMerge, ufNew, ufRoots)
 
 -- | An opaque class id
@@ -60,17 +56,17 @@ instance EAnalysis () f (EAnalysisOff f) where
   eaMake _ _ _ = ()
   eaModify _ _ g = g
 
--- | Constraints implemented by 'EGM'.
-type EGC d f q m = (MonadReader q m, MonadState (EGraph d f) m)
+-- -- | Constraints implemented by 'EGM'.
+-- type EGC d f q m = (MonadReader q m, MonadState (EGraph d f) m)
 
--- | The 'EGraph' monad - carries the definition of the analysis in the reader layer and the graph itself
--- in the state layer. Use it like 'm' in 'EGC d f q m' and use constraint 'EAnalysis d f q' when necessary.
--- Run with 'runEGM'.
-type EGM d f q = RSM q (EGraph d f)
+-- -- | The 'EGraph' monad - carries the definition of the analysis in the reader layer and the graph itself
+-- -- in the state layer. Use it like 'm' in 'EGC d f q m' and use constraint 'EAnalysis d f q' when necessary.
+-- -- Run with 'runEGM'.
+-- type EGM d f q = RSM q (EGraph d f)
 
--- | Runs the 'EGraph' monad.
-runEGM :: EGM d f q a -> q -> EGraph d f -> (a, EGraph d f)
-runEGM = runRSM
+-- -- | Runs the 'EGraph' monad.
+-- runEGM :: EGM d f q a -> q -> EGraph d f -> (a, EGraph d f)
+-- runEGM = runRSM
 
 -- | Info stored for every class: analysis data and class members.
 data EClassInfo d = EClassInfo
@@ -107,28 +103,16 @@ egNew :: EGraph d f
 egNew = EGraph (sourceNew (EClassId 0)) ufNew HashMap.empty (assocNew (ENodeId 0)) HashMap.empty HashSet.empty
 
 -- | Yields all root classes
-egClasses :: EGM d f q (HashSet EClassId)
+egClasses :: State (EGraph d f) (HashSet EClassId)
 egClasses = stateLens egUnionFindL ufRoots
 
 -- private
-egCanonicalize :: Traversable f => f EClassId -> EGM d f q (Maybe (f EClassId))
+egCanonicalize :: Traversable f => f EClassId -> State (EGraph d f) (Maybe (f EClassId))
 egCanonicalize = stateLens egUnionFindL . fmap sequence . traverse ufFind
 
 -- private
-egMake :: EAnalysis d f q => f EClassId -> EGM d f q d
-egMake fc = do
-  q <- ask
-  fmap (eaMake q fc) get
-
--- private
-egModify :: EAnalysis d f q => EClassId -> EGM d f q ()
-egModify x = do
-  q <- ask
-  modify' (eaModify q x)
-
--- private
-egAddNode :: (EAnalysis d f q, Eq (f EClassId), Hashable (f EClassId)) => f EClassId -> EGM d f q (Changed, EClassId)
-egAddNode fc = do
+egAddNode :: (EAnalysis d f q, Eq (f EClassId), Hashable (f EClassId)) => q -> f EClassId -> State (EGraph d f) (Changed, EClassId)
+egAddNode q fc = do
   (c, n) <- stateLens egNodeAssocL (assocEnsure fc)
   x <- case c of
         ChangedNo -> do
@@ -142,26 +126,26 @@ egAddNode fc = do
           -- map the node to the class id
           stateLens egHashConsL (modify' (HashMap.insert n x))
           -- analyze the node and put that info in the class map
-          d <- egMake fc
+          d <- fmap (eaMake q fc) get
           let i = EClassInfo d (HashSet.singleton n)
           stateLens egClassMapL (modify' (HashMap.insert x i))
           -- call analysis modify
-          egModify x
+          modify' (eaModify q x)
           pure x
   pure (c, x)
 
 -- | Adds a term (recursively) to the graph. If already in the graph, returns 'ChangedNo' and existing class id. Otherwise
 -- returns 'ChangedYes' and a new class id.
-egAddTerm :: (EAnalysis d f q, RecursiveWhole t f, Traversable f, Eq (f EClassId), Hashable (f EClassId)) => t -> EGM d f q (Changed, EClassId)
-egAddTerm = foldWholeTrackM egAddNode
+egAddTerm :: (EAnalysis d f q, RecursiveWhole t f, Traversable f, Eq (f EClassId), Hashable (f EClassId)) => q -> t -> State (EGraph d f) (Changed, EClassId)
+egAddTerm = foldWholeTrackM . egAddNode
 
 -- | Merges two classes:
 -- Returns 'Nothing' if the classes are not found
 -- Otherwise returns the merged class id and whether anything has changed
 -- If things have changed, then you must call 'egRebuild' before adding more terms.
 -- (You can use 'egNeedsRebuild' to query this.)
-egMerge :: EClassId -> EClassId -> EGM d f q (Maybe (Changed, EClassId))
-egMerge i j = do
+egMerge :: {- EAnalysis d f q => -} q -> EClassId -> EClassId -> State (EGraph d f) (Maybe (Changed, EClassId))
+egMerge _ i j = do
   -- merge classes in the uf
   -- TODO get old roots for i and j, and for changed ones, join their data and modify
   mx <- stateLens egUnionFindL (ufMerge i j)
@@ -175,6 +159,6 @@ egNeedsRebuild :: EGraph d f -> Bool
 egNeedsRebuild = not . HashSet.null . egWorkList
 
 -- | Rebuilds the 'EGraph' after merging to allow adding more terms. (Always safe to call.)
-egRebuild :: EGM d f q ()
+egRebuild :: {- EAnalysis d f q => -} q -> State (EGraph d f) ()
 egRebuild = error "TODO"
 -- TODO implement rebuild + repair from the paper (fig 9)
--- src/Overeasy/StateUtil.hs
+++ src/Overeasy/StateUtil.hs
@@ -3,18 +3,15 @@ module Overeasy.StateUtil
   ( stateFail
   , stateFailChanged
   , stateLens
-  , RSM
-  , runRSM
   ) where
 
-import Control.Monad.Reader (MonadReader, ReaderT (..), runReaderT)
-import Control.Monad.State.Strict (MonadState (..), State, runState)
+import Control.Monad.State.Strict (State, get, put, runState, state)
 import Lens.Micro (Lens', set)
 import Lens.Micro.Extras (view)
 import Overeasy.Classes (Changed (..))
 
 -- | Embeds a function that may fail in a stateful context
-stateFail :: MonadState s m => (s -> Maybe (b, s)) -> m (Maybe b)
+stateFail :: (s -> Maybe (b, s)) -> State s (Maybe b)
 stateFail f = do
   s <- get
   case f s of
@@ -22,7 +19,7 @@ stateFail f = do
     Just (b, s') -> put s' >> pure (Just b)
 
 -- | Embeds a function that may fail in a stateful context with change tracking
-stateFailChanged :: MonadState s m => (s -> Maybe s) -> m Changed
+stateFailChanged :: (s -> Maybe s) -> State s Changed
 stateFailChanged f = do
   s <- get
   case f s of
@@ -30,16 +27,8 @@ stateFailChanged f = do
     Just s' -> put s' >> pure ChangedYes
 
 -- | Embeds a stateful action in a larger context
-stateLens :: MonadState s m => Lens' s a -> State a b -> m b
+stateLens :: Lens' s a -> State a b -> State s b
 stateLens l act = state $ \s ->
   let (b, a') = runState act (view l s)
       s' = set l a' s
   in (b, s')
-
--- | A Reader-State monad
-newtype RSM r s a = RSM { unRSM :: ReaderT r (State s) a }
-  deriving newtype (Functor, Applicative, Monad, MonadReader r, MonadState s)
-
--- | Runs the Reader-State monad
-runRSM :: RSM r s a -> r -> s -> (a, s)
-runRSM (RSM m) = runState . runReaderT m
